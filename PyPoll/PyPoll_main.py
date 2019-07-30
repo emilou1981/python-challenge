2b59815d163f0d4de7e013ca1f3005ac286de3c6
Emily Fischer <emily.m.fischer@outlook.com>
2019-07-30 18:11:17 -0500

    updates to code

--- PyPoll/PyPoll_main.py
+++ PyPoll/PyPoll_main.py
@@ -10,6 +10,9 @@ import os
 import csv
 #declare starting values
 Votes_Cast = 0
+Candidate_Votes = []
+Candidate_Votes = 0
+Candidate_List = []
 
 # Path to collect data 
 election_csv = os.path.join("..", "RawData", "election_data.csv")
@@ -28,21 +31,20 @@ with open(election_csv, newline="") as csvfile:
 # Define the function and have it accept the 'election_data' as its sole parameter
         Voter_ID = int(row[0])
         County = str(row[1])
-        Candidate = str(row[2])
+        Candidate_Name = str(row[2])
 #The total number of votes cast
         Votes_Cast = Votes_Cast + 1
 
-#A complete list of candidates who received votes
-        Candidiates_List = []
-        Candidiates_List.append(row[2])
-        Candidiates_List = set(Candidate)
-        # Candidiates_List = list(set())
-        print(Candidiates_List)
-        
+        # Candidate_Votes[Candidate_Name]+=1
+
+# #A complete list of candidates who received votes
+          
+        Candidate_List.append(Candidate_Name) 
         
 #The percentage of votes each candidate won
 
-
+# for name in Candidate_Votes:
+#     print(f'{Candidate_Name}: {Candidate_Votes[name]}(, ({Votes_Cast} / {Candidate_Votes[name]}),)' )
 
 
 #The total number of votes each candidate won
@@ -53,10 +55,10 @@ with open(election_csv, newline="") as csvfile:
 
        
 
-
+    Unique_Candidate_List = set(Candidate_List)
 #     # Print out the results
     print("Election Results")
     print("------------------------------------")
     print(f'Total Votes: {Votes_Cast}')
     print("------------------------------------")
-    print(f'List of Candidates: {Candidiates_List}')
+    print(f'List of Candidates: {Unique_Candidate_List}')
