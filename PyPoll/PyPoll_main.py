30adf121ce6b695e1365f0d51a1398a9ad23581e
Emily Fischer <emily.m.fischer@outlook.com>
2019-08-01 08:19:33 -0500

    Final version of PyPoll updated

--- PyPoll/PyPoll_main.py
+++ PyPoll/PyPoll_main.py
@@ -50,26 +50,7 @@ with open(election_csv, newline="") as csvfile:
         else:
             d[Candidate_Name] = 1
 
-# #A complete list of candidates who received votes
-          
-        # Candidate_List.append(Candidate_Name) 
-        
-#The percentage of votes each candidate won
-        #print(d['Khan'])
-        #num_votes = d[Candidate_Name]
-        #for d[Candidate_Name] in d:
-            #print(d[Candidate_Name])
-        # #  #   Percentage_Won = (num_votes/Votes_Cast)*100
-        # for key.value in d.[Candidate_Name(row[2]):
-        # for key, value in d.items():
-        # for k, v in d.items():
-        # print(k, v)
-            # for key, value in d.items():
-                
-            #     # print(k,v)
-
-            #     Percentage_Won = (value/Votes_Cast)*100
-#finding winner of election
+#Determine winner of election
 for key in d.keys():
     if d[key] > winningTotal:
         winningTotal = d[key]
@@ -79,47 +60,16 @@ for key in d.keys():
 
 #calculate candidate percentages
 for key, Votes in d.items():
-    Win_Percent[key] = (Votes/Votes_Cast)*100
+    Win_Percent[key] = round((Votes/Votes_Cast)*100,3)
     # print(Win_Percent[key])   
     
-
-    # if d.[key] > winningTotal:
-    #     winningTotal = d[key]
-    #     Winner = key
-  
-
-# for key in d.keys():
-#     if d
-#     Percentage_Won = (float(key)/Votes_Cast)*100
-        
-# for name in Candidate_Votes:
-
-
-#The total number of votes each candidate won
-
-
-
-#The winner of the election based on popular vote.     
-
-       
-
-    # Unique_Candidate_List = set(Candidate_List)
-#     # Print out the results
-
 
 print("Election Results")
 print("------------------------------------")
 print(f'Total Votes: {Votes_Cast}')
 print("------------------------------------")
 for key in d.keys():
-    print(f'{key}: {Win_Percent[key]} ({d[key]})')
+    print(f'{key}: {Win_Percent[key]}% ({d[key]})')
 print("------------------------------------")   
 print(f'Winner: {Winner}')
 print("------------------------------------")   
-# print(f'List of Candidates: {d}')
-    # for d[Candidate_Name] in d:
-    #         #print(d[Candidate_Name])
-    #     print(int(d[Candidate_Name]))
-      #Percentage_Won = (int(d[Candidate_Name])/Votes_Cast)*100
-    # print(int(d[Candidate_Name]))
-    # print({Percentage_Won})
