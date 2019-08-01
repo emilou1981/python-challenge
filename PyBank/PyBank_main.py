286505d70dabf817cbad9267686b8b7a4e294dca
Emily Fischer <emily.m.fischer@outlook.com>
2019-08-01 16:02:43 -0500

    updates

--- PyBank/PyBank_main.py
+++ PyBank/PyBank_main.py
@@ -10,18 +10,47 @@
 
 import os
 import csv
+
 #declare starting value of total net profit
 total_net_profit = 0
 total_months = 0
 Month_Change = 0
 Average_Change = []
 Average_Change = 0
-Previous_Month_PL= []
+Previous_Month_PL= {}
+Previous_Date = ""
+Prior_Month_Change = []
+Profit_Loses = []
+
+
+
+
 
 # Path to collect data 
 bank_csv = os.path.join("..", "RawData", "budget_data.csv")
 
-#d
+#loop through data first time to establish list for prior profit/loss
+with open(bank_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # Read the header row first (skip this part if there is no header)
+    csv_header = next(csvfile)
+    skip_2nd = next(csvfile)
+  
+    # Read through each row of data after the header
+    for row in csvreader:
+# Define the function and have it accept the 'budget_data' as its sole parameter
+        # def print_financials(BankBudget_data):
+
+        Previous_Date = str(row[0])
+        Previous_Month_PL = int(row[1])
+        # print(f'{Previous_Date} : {Previous_Month_PL}')
+
+
+#-------------------------------------------------------------------
+# Second loop
+# Path to collect data 
+bank_csv = os.path.join("..", "RawData", "budget_data.csv")
 # Open and read csv
 with open(bank_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -33,30 +62,27 @@ with open(bank_csv, newline="") as csvfile:
     for row in csvreader:
 
 # Define the function and have it accept the 'budget_data' as its sole parameter
-        # def print_financials(BankBudget_data):
-
-            Date = str(row[0])
-            Profit_Loses = int(row[1])
     
-            # Find the total number of months included in the dataset
-            total_months = total_months + 1
 
-            #the net total amount of "Profit/Losses" over the entire period
-            total_net_profit = total_net_profit + Profit_Loses
+        Date = str(row[0])
+        Profit_Loses = int(row[1])
 
-            #define average to calculate average change
-            try:
-                Previous_Month_PL
-            except NameError:
-                Previous_Month_PL = int(row[1]) 
+        # Find the total number of months included in the dataset
+        total_months = total_months + 1
 
-        #        def average(numbers):
-        #         length = len(numbers)
-        #         total = 0.0
-        #         for number in numbers:
-        #         total += number
-        #         return total / length 
-            
+        #the net total amount of "Profit/Losses" over the entire period
+        total_net_profit = total_net_profit + Profit_Loses
+
+#The average of the changes in "Profit/Losses" over the entire period
+       
+        # Prior_Month_Change = Profit_Loses
+        Prior_Month_Change = ((Previous_Month_PL - Profit_Loses))
+
+        print(f'Prior Month Change: {Prior_Month_Change}')
+
+# Average_Change = sum(Prior_Month_Change)/(total_months - 1)
+
+
            
             
             # Average_Change = round((total_net_profit / total_months),2)
@@ -64,13 +90,15 @@ with open(bank_csv, newline="") as csvfile:
             #     Incr_Profits = 
             # #The greatest decrease in losses (date and amount) over the entire period
             #     Decr_Profits = 
-    Average_Change = (Previous_Month_PL-Average_Change)
+
+
 #     # Print out the financial analysis
-    print("Financial Analysis")
-    print("------------------------------------")
-    print(f'Total Months: {total_months}')
-    print(f'Total: ${total_net_profit}')
-    print(f'Average Monthly Change: {Previous_Month_PL}')
+print("Financial Analysis")
+print("------------------------------------")
+print(f'Total Months: {total_months}')
+print(f'Total: ${total_net_profit}')
+print(f'Average Change: {Prior_Month_Change}')
+    # print(f'Average Monthly Change: {Previous_Month_PL}')
     # print(f'Average Change: ${Average_Change}')
     # print(f'Greatest Increase in Profits: {Date} {Incr_Profits}')
     # print(f'Greatest Increase in Profits: {Date} {Decr_Profits}')
--- PyPoll/PyPoll_main.py
+++ PyPoll/PyPoll_main.py
@@ -6,24 +6,25 @@
     #The winner of the election based on popular vote.
     #In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
+
 import os
 import csv
+
 #declare starting values
 Votes_Cast = 0
-Votes = []
-Votes = 0
-# Candidate_List = []
-Received_Votes = []
-Percentage_Won = []
+
+    #dictionary to calc winning percent
 Win_Percent = {}
+
+    #dictionay to compile total votes received by candidates
 d = {}
-num_votes = 0
+
+    #varibles to determining winner based on totals
 winningTotal = 0
 Winner = ""
 
 # Path to collect data 
 election_csv = os.path.join("..", "RawData", "election_data.csv")
-
 
 # Open and read csv
 with open(election_csv, newline="") as csvfile:
@@ -35,15 +36,13 @@ with open(election_csv, newline="") as csvfile:
     # Read through each row of data after the header
     for row in csvreader:
 
-# Define the function and have it accept the 'election_data' as its sole parameter
-        Voter_ID = int(row[0])
-        County = str(row[1])
+# Define the candidate name from CSV file
         Candidate_Name = str(row[2])
 
-#The total number of votes cast
+#Count the total number of votes cast
         Votes_Cast = Votes_Cast + 1
         
-# create dictionary and count candidate votes
+# create dictionary and count candidate votes received
         
         if Candidate_Name in d:
             d[Candidate_Name] = d[Candidate_Name] + 1
@@ -62,12 +61,14 @@ for key in d.keys():
 for key, Votes in d.items():
     Win_Percent[key] = round((Votes/Votes_Cast)*100,3)
     # print(Win_Percent[key])   
+   
     
-
+#prit out data for output
 print("Election Results")
 print("------------------------------------")
 print(f'Total Votes: {Votes_Cast}')
 print("------------------------------------")
+#write out print string referencing the keys from d and win_percent dictionaries
 for key in d.keys():
     print(f'{key}: {Win_Percent[key]}% ({d[key]})')
 print("------------------------------------")   
